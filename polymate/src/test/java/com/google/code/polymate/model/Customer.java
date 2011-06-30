1114f6dbcef2cf6cf1e125ce9f1112694ce8cb3d
mai.marc@gmail.com <mai.marc@gmail.com@8b37662d-a4ed-cf3c-c53b-0e4e86014430>
2011-06-30 19:24:44 +0000

    initial src import

---
+++ polymate/src/test/java/com/google/code/polymate/model/Customer.java
@@ -0,0 +1,62 @@
+/*
+ *	Copyright (c) 2011 Marc Mai
+ *
+ *	Licensed under the MIT license: 
+ *	http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+package com.google.code.polymate.model;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.neo4j.graphdb.Node;
+
+import com.google.code.morphia.annotations.Embedded;
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Id;
+import com.google.code.morphia.annotations.Transient;
+import com.google.code.polymate.UnderlyingNode;
+
+/**
+ * 
+ * TODO javadoc
+ * 
+ * <br>
+ * <br>
+ * <small> Copyright (c) 2011 Marc Mai <br />
+ * https://code.google.com/p/polymate/ </small> <br>
+ * 
+ * @author mai.marc@gmail.com<br>
+ *         </i></small>
+ */
+@Entity
+@SuppressWarnings("unused")
+public class Customer {
+
+	@Id
+	private ObjectId id;
+	private String name;
+	@UnderlyingNode
+	@Transient
+	private Node node;
+
+	private List<Order> orders;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+
+}
